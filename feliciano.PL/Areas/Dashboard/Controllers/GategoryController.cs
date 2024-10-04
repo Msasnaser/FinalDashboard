92f8f379817194aa014ea29a0ac409812ca088e4
Masanaser <masadawood29@gmail.com>
2024-10-04 18:25:53 +0300

    Add project files.

---
+++ feliciano.PL/Areas/Dashboard/Controllers/GategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace feliciano.PL.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class GategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ feliciano.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace feliciano.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
